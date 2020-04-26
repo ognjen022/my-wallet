a41aa7003d26355e666adbeefcc61354c6cd1a78
ognjen022 <ognjen022@gmail.com>
2020-04-26 17:34:24 +0200

    server refactor

--- server/server.js
+++ server/server.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
 
-app.use(express.static(publicPath));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(publicPath, 'index.html'));
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port, () => {
